4f81f8c5b9612dd33d7a27f08c0b30c30d7608c7
Homsi <mlhomsi@gmail.com>
2018-02-03 14:51:58 -0200

    Logica dos Portais

--- GJsom/GJsom/Assets/Scripts/Connect.cs
+++ GJsom/GJsom/Assets/Scripts/Connect.cs
@@ -10,20 +10,47 @@ using UnityEngine;
  * Quarto = 7;
  */
 
-public class Connect : MonoBehaviour {
+public class Connect : MonoBehaviour
+{
 
     public string location;
-    static int rank;
-    static int i=0;
+    static int rank = 0;
+    static int insanidade = 0;
+    public int comodo;
 
     private void OnTriggerEnter(Collider other)
     {
-        i++;
-        Debug.Log(location + " " + i);
+        insanidade++;
+        rank += comodo;
+        Debug.Log(location + " " + rank);
+        CasaFantasma01();
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void CasaFantasma01()
+    {
+        if (location == "Cozinha")
+        { if(rank == 26)
+            { Debug.Log("Vai para Banheiro da casa fantasma"); }
+        }
+
+        if (location == "Banheiro")
+        {
+            if (rank == 29)
+            { Debug.Log("Vai para Sala da casa fantasma"); }
+            else Debug.Log("Vai para Quarto da casa fantasma");
+        }
+
+        if (location == "Quarto")
+        {
+            if (rank == 31)
+            { Debug.Log("Vai para Cozinha da casa fantasma"); }
+            else Debug.Log("Vai para Sala da casa fantasma");
+        }
+
+        if (location == "Sala")
+        {
+            if (rank == 29 || rank == 31)
+            { Debug.Log("Vai para Sala da casa fantasma"); }
+        }
+    }
 }
