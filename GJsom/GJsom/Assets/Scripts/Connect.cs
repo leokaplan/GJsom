518a7cf23d3046e5893d3640c79e80fc2ccd2d07
Homsi <mlhomsi@gmail.com>
2018-02-03 13:00:30 -0200

    Teste de Tracker do player

---
+++ GJsom/GJsom/Assets/Scripts/Connect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connect : MonoBehaviour {
+
+    public string location;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(location);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
