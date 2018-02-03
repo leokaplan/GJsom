a62802db3b34dac42ae30b46857deab7e0c11dee
desktop\Kaplan <leonardokkaplan@gmail.com>
2018-02-03 20:27:50 -0200

    novo player

---
+++ GJsom/GJsom/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public AnimationCurve ComodosXInsanidade;
+	public int insanidade;
+	int comodosVisitados = 0;
+	private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag=="Portal"){
+			comodosVisitados++;
+			UpdateInsanidade();
+		}
+        
+    }
+	void UpdateInsanidade(){
+		insanidade = Mathf.FloorToInt(ComodosXInsanidade.Evaluate(Mathf.FloorToInt(comodosVisitados)));
+	}
+	void Start () {
+		UpdateInsanidade();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
