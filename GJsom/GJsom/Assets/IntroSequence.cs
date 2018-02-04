caea7295c527d9eb3dd1faac3da6a823c3d147a9
Homsi <mlhomsi@gmail.com>
2018-02-04 01:28:02 -0200

    Start Scene

---
+++ GJsom/GJsom/Assets/IntroSequence.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSequence : MonoBehaviour {
+
+    public GameObject ui;
+
+
+	void Start () {
+        StartCoroutine(start());
+	}
+
+    IEnumerator start()
+    {
+        yield return new WaitForSecondsRealtime(3.0f);
+        ui.SetActive(true);
+    }
+}
---
+++ GJsom/GJsom/Assets/Scripts/IntroManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+
+    public AudioClip intro;
+    public GameObject title;
+
+
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
