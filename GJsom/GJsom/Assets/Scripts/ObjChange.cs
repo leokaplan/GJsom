4cd5b5a6e44a75706e66d503e5b48e80f0c85fc8
desktop\Kaplan <leonardokkaplan@gmail.com>
2018-02-04 16:25:59 -0200

    teleport

---
+++ GJsom/GJsom/Assets/Scripts/ObjChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjChange : MonoBehaviour {
+
+	// Use this for initialization
+	Player Player;
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- GJsom/GJsom/Assets/Scripts/Player.cs
+++ GJsom/GJsom/Assets/Scripts/Player.cs
@@ -7,22 +7,37 @@ public class Player : MonoBehaviour {
 	public AnimationCurve ComodosXInsanidade;
 	public int insanidade;
 	int comodosVisitados = 0;
+	public List<int> InsanidadePTeleport;
+	public List<GameObject> Casas;
+	int TPi;
 	private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.tag=="Portal"){
+        if(other.transform.tag=="Comodo"){
 			comodosVisitados++;
 			UpdateInsanidade();
 		}
         
     }
+	IEnumerator teleport(){
+		Casas[TPi].SetActive(true);
+		Vector3 pos = transform.localPosition;
+		transform.parent = Casas[TPi].transform;
+		transform.localPosition = pos;
+		Casas[TPi-1].SetActive(false);
+		yield return new WaitForSeconds(0.001f);
+	}
 	void UpdateInsanidade(){
 		insanidade = Mathf.FloorToInt(ComodosXInsanidade.Evaluate(Mathf.FloorToInt(comodosVisitados)));
+		if(TPi!=InsanidadePTeleport.Count && insanidade==InsanidadePTeleport[TPi]){
+			TPi++;
+			StartCoroutine(teleport());
+		}
 	}
 	void Start () {
+		TPi = 0;
 		UpdateInsanidade();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
