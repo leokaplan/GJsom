3bf814843912c589d611451dc8d2b46b54d6f929
Homsi <mlhomsi@gmail.com>
2018-02-05 02:19:59 -0200

    Balance 04.....05? ...Nao lembro

---
+++ GJsom/GJsom/Assets/Get.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Get : MonoBehaviour {
+
+    public GameObject foto;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("tocou");
+       // if (Input.GetKeyDown("e"))
+       //     Destroy(foto);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
